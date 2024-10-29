e8d7da6b0deecdc0e4b44a860d9b12558e3881f0
suzuki.i <60678580+yohakubunka@users.noreply.github.com>
2024-10-29 11:10:03 +0900

    shadCNの追加

--- frontend/hoikuru/app/page.tsx
+++ frontend/hoikuru/app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
     <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+
+    <div>
+      <Button>Click me</Button>
+    </div>
   </div>
   );
 }
