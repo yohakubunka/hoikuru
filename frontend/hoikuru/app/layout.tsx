af4c03f6abcf4df80adb70dc0e8db6a62650148b
furuta-r-yohaku <151109646+furuta-r-yohaku@users.noreply.github.com>
2024-12-24 19:45:48 +0900

    sidebar create

--- frontend/hoikuru/app/layout.tsx
+++ frontend/hoikuru/app/layout.tsx
@@ -1,4 +1,4 @@
-import DeployButton from "@/components/deploy-button";
+import { cookies } from "next/headers"
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
@@ -21,11 +21,16 @@ export const metadata = {
   description: "The fastest way to build apps with Next.js and Supabase",
 };
 
-export default function RootLayout({
+const SIDEBAR_WIDTH = "16rem"
+const SIDEBAR_WIDTH_MOBILE = "18rem"
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+    const cookieStore = await cookies()
+  const defaultOpen = cookieStore.get("sidebar:state")?.value === "true"
   return (
     <html lang="ja" className={GeistSans.className} suppressHydrationWarning>
       <body className="bg-background text-foreground">
@@ -35,41 +40,39 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-              <SidebarProvider>
-              <AppSidebar />
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
+          <SidebarProvider defaultOpen={defaultOpen}
+          >
+            <AppSidebar />
+            <main className="min-h-screen w-full flex flex-col items-center">
+              <div className="flex-1 w-full flex flex-col gap-20 items-center relative">
+                <div className="absolute top-2 left-2"><SidebarTrigger /></div>
+                <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+                  <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                    <div className="flex gap-5 items-center font-semibold">
+                      <Link href={"/"}>Next.js Supabase Starter</Link>
+                    </div>
+                    {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                   </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                </nav>
+                <div className="flex flex-col gap-20 max-w-5xl p-5">
+                  {children}
                 </div>
-              </nav>
-
-
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-              <SidebarTrigger />
-                {children}
+                <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+                  <p>
+                    Powered by{" "}
+                    <a
+                      href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+                      target="_blank"
+                      className="font-bold hover:underline"
+                      rel="noreferrer"
+                    >
+                      Supabase
+                    </a>
+                  </p>
+                  <ThemeSwitcher />
+                </footer>
               </div>
-
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
-            </div>
-          </main>
+            </main>
           </SidebarProvider>
           <Toaster />
         </ThemeProvider>
--- frontend/hoikuru/components/app-sidebar.tsx
+++ frontend/hoikuru/components/app-sidebar.tsx
@@ -1,116 +1,136 @@
-import { Edit, FileImage, Layout, Mail, Home, Inbox, Settings } from "lucide-react"
+"use client"
+import * as React from "react"
+import {
 
-// サイドバー
+  Pencil,
+  Home,
+  FileImage,
+  Layout,
+  Mail,
+  Settings,
+} from "lucide-react"
+import { NavMain } from "@/components/nav-main"
+import { NavProjects } from "@/components/nav-projects"
+import { NavUser } from "@/components/nav-user"
+import { TeamSwitcher } from "@/components/team-switcher"
 import {
   Sidebar,
   SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
+  SidebarFooter,
   SidebarHeader,
-  SidebarFooter
+  SidebarRail,
 } from "@/components/ui/sidebar"
-
-// ドロップダウンメニュー
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-
 // アバター
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
-// Menu items.
-const items = [
-  {
-    title: "ダッシュボード",
-    url: "#",
-    icon: Home,
-  },
-  {
-    title: "お客様情報",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "投稿",
-    url: "#",
-    icon: Edit,
-  },
-  {
-    title: "メディア",
-    url: "#",
-    icon: FileImage,
-  },
-  {
-    title: "ページ編集",
-    url: "#",
-    icon: Layout,
-  },
-  {
-    title: "お問い合わせ",
-    url: "#",
-    icon: Mail,
-  },
-  {
-    title: "設定",
-    url: "#",
-    icon: Settings,
-  },
-]
 
-export function AppSidebar() {
+
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "ダッシュボード",
+      url: "#",
+      icon: Home
+    },
+    {
+      title: "お客様情報",
+      url: "#",
+      icon: Pencil,
+    },
+    {
+      title: "投稿",
+      url: "#",
+      icon: Pencil,
+      items: [
+        {
+          title: "新規投稿",
+          url: "#",
+        },
+        {
+          title: "投稿一覧",
+          url: "#",
+        },
+        {
+          title: "カテゴリー",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "メディア",
+      url: "#",
+      icon: FileImage,
+      items: [
+        {
+          title: "サンプル",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "ページ編集",
+      url: "#",
+      icon: Layout,
+      items: [
+        {
+          title: "サンプル",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "お問い合わせ",
+      url: "#",
+      icon: Mail,
+      items: [
+        {
+          title: "サンプル",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "設定",
+      url: "#",
+      icon: Settings,
+      items: [
+        {
+          title: "サンプル",
+          url: "#",
+        },
+      ],
+    },
+  ],
+}
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar>
-      <SidebarContent>
-      <SidebarHeader className="mb-4">
-            <div className="flex gap-4 items-center">
-              <Avatar className="flex-none">
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-              <div className="flex-1 grow">
-                <p>ホイクル園</p>
-                <a href="">sssssss</a>
-              </div>
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+      <div className="flex gap-4 p-2 items-center">
+            <Avatar className="flex-none">
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div className="flex-1 grow">
+              <p>ホイクル園</p>
+              <a className="text-slate-400 text-sm" href="/.">サイト表示</a>
             </div>
-          </SidebarHeader>
-        <SidebarGroup>
-         
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-        <SidebarFooter>
-          <DropdownMenu>
-            <DropdownMenuTrigger>アカウント</DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>プロフィール</DropdownMenuItem>
-              <DropdownMenuItem>パスワード変更</DropdownMenuItem>
-              <DropdownMenuItem>ログアウト</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </SidebarFooter>
+          </div>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
       </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
     </Sidebar>
   )
 }
